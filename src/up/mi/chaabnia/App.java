7e616e58877efa84028e2b2ed841b1694f84e012
akramch <akram.chaabnia25@gmail.com>
2023-12-25 19:59:54 +0100

    tp7 partie B

---
+++ src/up/mi/chaabnia/App.java
@@ -0,0 +1,86 @@
+package up.mi.chaabnia;
+
+import up.mi.chaabnia.WeightedGraph.Edge;
+import up.mi.chaabnia.WeightedGraph.Graph;
+import up.mi.chaabnia.WeightedGraph.Vertex;
+
+import java.util.*;
+
+public class App {
+	private static LinkedList<Integer> Dijkstra(Graph graph, int start, int end, int numberV) {
+		PriorityQueue<Vertex> to_visit = new PriorityQueue<>(numberV, Comparator.comparingDouble(v -> v.timeFromSource));
+
+		for (int i = 0; i < numberV; i++) {
+			graph.vertexlist.get(i).timeFromSource = Double.MAX_VALUE;
+			to_visit.add(graph.vertexlist.get(i));
+		}
+
+		graph.vertexlist.get(start).timeFromSource = 0;
+
+		int number_tries = 0;
+
+		while (!to_visit.isEmpty()) {
+			Vertex min_v = to_visit.poll();
+			if (min_v == null) {
+				System.out.println("No minimum vertex found!");
+				break;
+			}
+
+			number_tries++;
+
+			for (Edge edge : min_v.adjacencylist) {
+				int to_try = edge.destination;
+				double new_dist = min_v.timeFromSource + edge.weight;
+
+				if (new_dist < graph.vertexlist.get(to_try).timeFromSource) {
+					graph.vertexlist.get(to_try).timeFromSource = new_dist;
+					graph.vertexlist.get(to_try).prev = min_v;
+
+					to_visit.remove(graph.vertexlist.get(to_try));
+					to_visit.add(graph.vertexlist.get(to_try));
+				}
+			}
+		}
+
+		LinkedList<Integer> path = new LinkedList<>();
+		path.addFirst(end);
+		int current = end;
+		while (current != start) {
+			Vertex prevVertex = graph.vertexlist.get(current).prev;
+			if (prevVertex != null) {
+				current = prevVertex.num;
+				path.addFirst(current);
+			} else {
+				System.out.println("No path found from start to end node.");
+				break;
+			}
+		}
+		return path;
+	}
+
+	public static void main(String[] args) {
+		Graph graph = new Graph();
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addEdge(0, 1, 4);
+		graph.addEdge(0, 2, 3);
+		graph.addEdge(1, 3, 2);
+		graph.addEdge(1, 2, 5);
+		graph.addEdge(2, 3, 7);
+		graph.addEdge(3, 4, 2);
+		graph.addEdge(4, 0, 4);
+		graph.addEdge(4, 1, 4);
+		graph.addEdge(4, 5, 6);
+
+		int startNode = 0;
+		int endNode = 5;
+
+		LinkedList<Integer> shortestPath = Dijkstra(graph, startNode, endNode, graph.num_v);
+		System.out.println("Shortest path from node " + startNode + " to node " + endNode + ": " + shortestPath);
+	}
+
+}
---
+++ src/up/mi/chaabnia/WeightedGraph.java
@@ -0,0 +1,68 @@
+package up.mi.chaabnia;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class WeightedGraph {
+    static class Edge {
+        int source;
+        int destination;
+        double weight;
+
+        public Edge(int source, int destination, double weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    static class Vertex {
+        double indivTime;
+        double timeFromSource;
+        Vertex prev;
+        LinkedList<Edge> adjacencylist;
+        int num;
+
+        public Vertex() {
+            this.indivTime = Double.POSITIVE_INFINITY;
+            this.timeFromSource = Double.POSITIVE_INFINITY;
+            this.prev = null;
+            this.adjacencylist = new LinkedList<>();
+            this.num = -1;
+        }
+
+        public Vertex(int num) {
+            this.indivTime = Double.POSITIVE_INFINITY;
+            this.timeFromSource = Double.POSITIVE_INFINITY;
+            this.prev = null;
+            this.adjacencylist = new LinkedList<>();
+            this.num = num;
+        }
+
+        public void addNeighbor(Edge e) {
+            this.adjacencylist.addFirst(e);
+        }
+    }
+
+    static class Graph {
+        ArrayList<Vertex> vertexlist;
+        int num_v = 0;
+
+        Graph() {
+            vertexlist = new ArrayList<>();
+        }
+
+        public void addVertex(double indivTime) {
+            Vertex v = new Vertex(num_v);
+            v.indivTime = indivTime;
+            vertexlist.add(v);
+            num_v++;
+        }
+
+        public void addEdge(int source, int destination, double weight) {
+            Edge edge = new Edge(source, destination, weight);
+            vertexlist.get(source).addNeighbor(edge);
+        }
+    }
+
+}
